53fb11cc0da273f17331183722e3463fe60c91ca
oprovakor <78918260+oprovakor@users.noreply.github.com>
2021-02-11 23:03:41 +0600

    change one by pk
    decreasing first picture sliding time

--- resource/js/slide.js
+++ resource/js/slide.js
@@ -1,6 +1,10 @@
     // start Slider=========
 
-    var counter = 1;
+
+    var firstshow = 0;
+    var time = 500;
+    document.getElementById('radio' + 2).checked = true;
+    var counter = 3;
     setInterval(function(){
         document.getElementById('radio' + counter).checked = true;
         counter ++;
